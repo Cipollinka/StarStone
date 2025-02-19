798f4645169ecc2993b570248d147e30d1108db5
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-02-19 23:15:53 +0200

    change date

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = 'cea9c834-25fe-4ff9-94a1-39ce0741014d'; // ключ OneSignal
   static appID = '6738917148'; // ID додатку в сторі
   static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
-  static targetDate = '2025-02-18'; // дата закінчення відльожки
+  static targetDate = '2025-02-21'; // дата закінчення відльожки
   static bodyLin = 'https://miraculous-imperial-happiness.space/zcfwMLJN'; // тіло лінки з якою будемо працювати
 }
